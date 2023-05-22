ca08384cad472bef777b20b2edb7050eb2258430
Juliana Cerqueira <juliana.cerqueira55@gmail.com>
2023-05-22 19:28:52 -0300

    Add files via upload

--- Autenticacao/script.js
+++ Autenticacao/script.js
@@ -2,12 +2,41 @@ function validarFormulario (event) {
     event.preventDefault();
     
     const email = window.document.getElementById('email');
-    const password = window.document.getElementById('password').value;
-    const confirmPassword = window.document.getElementById("confirmPassword").value;
-
-    console.log('valores : ', email, password, confirmPassword);
+    const password = window.document.getElementById('password');
+    const confirmPassword = window.document.getElementById("confirmPassword");
     
+    const messageEmail= window.document.getElementsByClassName("message-email");
+    const messagePassword = window.document.getElementsByClassName('message-password');
+    const messageConfirmPassword = window.document.getElementsByClassName('message-confirmPassword')
+    const messageCheckPassword = window.document.getElementsByClassName('message-fields-pswd')
+
     if(email.value == '') {
         email.classList.add('error');
+        messageEmail[0].classList.add('mostra-a-mensagem-de-erro');
+    } else {
+        email.classList.remove('error');
+        messageEmail[0].classList.remove('mostra-a-mensagem-de-erro')
+    }
+
+    if(password.value == '') {
+        password.classList.add('error');
+        messagePassword[0].classList.add('mostra-a-mensagem-de-erro')
+    } else {
+        password.classList.remove('error');
+        messagePassword[0].classList.remove('mostra-a-mensagem-de-erro')
+    }
+
+    if(confirmPassword.value == '') {
+        confirmPassword.classList.add('error');
+        messageConfirmPassword[0].classList.add('mostra-a-mensagem-de-erro')
+    } else {
+        password.classList.remove('error');
+        messagePassword[0].classList.remove('mostra-a-mensagem-de-erro')
+    }
+
+
+
+    if(password.value != confirmPassword.value) {
+        messageCheckPassword[0].classList.add('mostra-a-mensagem-de-erro')
     }
 }
